097618435cf56d35b567654d23e22eeac4b5f0ff
Semykin Vladislav <SemykinVD@Lumex.ru>
2025-07-30 11:03:31 +0300

    feat: added assertion macro, replaced utility from Interface target to Source in CMakeLists
    Signed-off-by: Semykin Vladislav <SemykinVD@Lumex.ru>

--- lumex/core/utility/LumexMacros.hpp
+++ lumex/core/utility/LumexMacros.hpp
@@ -1,10 +1,14 @@
 #ifndef LUMEX_MACROS_HPP
 #define LUMEX_MACROS_HPP
 
+#include "lumex/core/utility/LumexAssert.hpp"
+
 #define LUMEX_STRINGIZE_DETAIL(x) #x
 #define LUMEX_STRINGIZE(x) LUMEX_STRINGIZE_DETAIL(x)
 
 #define LUMEX_CONCAT_DETAIL(x, y) x##y
 #define LUMEX_CONCAT(x, y) LUMEX_CONCAT_DETAIL(x, y)
+
+#define LUMEX_ASSERT(cond) ((cond) ? static_cast<void>(0) : lumex_assert_handler(#cond, __FILE__, __LINE__))
 
 #endif // !LUMEX_MACROS_HPP
