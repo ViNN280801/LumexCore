c6ef59aa623566911db14226f38b5f89f74a6c29
ViNN280801 <vladislav_semykin01@mail.ru>
2025-08-01 09:10:52 +0300

    feat: implemented xml buffered writer using new encoding utilities
    Signed-off-by: ViNN280801 <vladislav_semykin01@mail.ru>

---
+++ lumex/xml/writer/XmlBufferedWriter.cpp
@@ -0,0 +1,200 @@
+#include "lumex/core/utility/LumexMacros.hpp"
+
+#include "lumex/xml/utility/XmlUtils.hpp"
+
+#include "XmlBufferedWriter.hpp"
+
+using namespace Lumex::Xml::Writer;
+using namespace Lumex::Xml::Utility;
+
+XmlBufferedWriter::XmlBufferedWriter(IXmlWriter &writer_, // NOLINT(cppcoreguidelines-pro-type-member-init)
+                                     xml_encoding user_encoding)
+    : writer(writer_), encoding(Utility::get_write_encoding(user_encoding))
+{
+  static_assert(bufcapacity >= kBufCapacity8);
+}
+
+size_t
+XmlBufferedWriter::flush()
+{
+  flush(buffer.data(), bufsize);
+  bufsize = 0;
+  return 0;
+}
+
+void
+XmlBufferedWriter::flush(char_t const *data, size_t size)
+{
+  if(size == 0) return;
+
+  // fast path, just write data
+  if(encoding == get_write_native_encoding())
+    writer.write(data, size * sizeof(char_t));
+  else
+  {
+    // convert chunk
+    size_t result
+      = Utility::convert_buffer_output(scratch.data_char.data(), // NOLINT(cppcoreguidelines-pro-type-union-access)
+                                       scratch.data_u8.data(),   // NOLINT(cppcoreguidelines-pro-type-union-access)
+                                       scratch.data_u16.data(),  // NOLINT(cppcoreguidelines-pro-type-union-access)
+                                       scratch.data_u32.data(),  // NOLINT(cppcoreguidelines-pro-type-union-access)
+                                       data, size, encoding);
+    LUMEX_ASSERT(result <= sizeof(scratch));
+
+    // write data
+    writer.write(scratch.data_u8.data(), result); // NOLINT(cppcoreguidelines-pro-type-union-access)
+  }
+}
+
+void
+XmlBufferedWriter::write_direct(char_t const *data, size_t length)
+{
+  // flush the remaining buffer contents
+  flush();
+
+  // handle large chunks
+  if(length > bufcapacity)
+  {
+    if(encoding == get_write_native_encoding())
+    {
+      // fast path, can just write data chunk
+      writer.write(data, length * sizeof(char_t));
+      return;
+    }
+
+    // need to convert in suitable chunks
+    while(length > bufcapacity)
+    {
+      // get chunk size by selecting such number of characters that are guaranteed to fit into scratch buffer
+      // and form a complete codepoint sequence (i.e. discard start of last codepoint if necessary)
+      size_t chunk_size = get_valid_length(data, bufcapacity);
+      LUMEX_ASSERT(chunk_size);
+
+      // convert chunk and write
+      flush(data, chunk_size);
+
+      // iterate
+      data += chunk_size;
+      length -= chunk_size;
+    }
+
+    // small tail is copied below
+    bufsize = 0;
+  }
+
+  memcpy(buffer.data() + bufsize, data, length * sizeof(char_t));
+  bufsize += length;
+}
+
+void
+XmlBufferedWriter::write_buffer(char_t const *data, size_t length)
+{
+  size_t offset = bufsize;
+
+  if(offset + length <= bufcapacity)
+  {
+    memcpy(buffer.data() + offset, data, length * sizeof(char_t));
+    bufsize = offset + length;
+  }
+  else { write_direct(data, length); }
+}
+
+void
+XmlBufferedWriter::write_string(char_t const *data)
+{
+  // write the part of the string that fits in the buffer
+  size_t offset = bufsize;
+
+  while((*data != 0) && offset < bufcapacity) buffer.at(offset++) = *data++;
+
+  // write the rest
+  if(offset < bufcapacity) { bufsize = offset; }
+  else
+  {
+    // backtrack a bit if we have split the codepoint
+    size_t length = offset - bufsize;
+    size_t extra  = length - get_valid_length(data - length, length);
+
+    bufsize       = offset - extra;
+
+    write_direct(data - extra, Utility::strlength(data) + extra);
+  }
+}
+
+void
+XmlBufferedWriter::write(char_t d0_)
+{
+  size_t offset = bufsize;
+  if(offset > bufcapacity - 1) offset = flush();
+
+  buffer.at(offset + 0) = d0_;
+  bufsize               = offset + 1;
+}
+
+void
+XmlBufferedWriter::write(char_t d0_, char_t d1_) // NOLINT(bugprone-easily-swappable-parameters)
+{
+  size_t offset = bufsize;
+  if(offset > bufcapacity - 2) offset = flush();
+
+  buffer.at(offset + 0) = d0_;
+  buffer.at(offset + 1) = d1_;
+  bufsize               = offset + 2;
+}
+
+void
+XmlBufferedWriter::write(char_t d0_, char_t d1_, char_t d2_) // NOLINT(bugprone-easily-swappable-parameters)
+{
+  size_t offset = bufsize;
+  if(offset > bufcapacity - 3) offset = flush();
+
+  buffer.at(offset + 0) = d0_;
+  buffer.at(offset + 1) = d1_;
+  buffer.at(offset + 2) = d2_;
+  bufsize               = offset + 3;
+}
+
+void
+XmlBufferedWriter::write(char_t d0_, char_t d1_, // NOLINT(bugprone-easily-swappable-parameters)
+                         char_t d2_, char_t d3_)
+{
+  size_t offset = bufsize;
+  if(offset > bufcapacity - 4) offset = flush();
+
+  buffer.at(offset + 0) = d0_;
+  buffer.at(offset + 1) = d1_;
+  buffer.at(offset + 2) = d2_;
+  buffer.at(offset + 3) = d3_;
+  bufsize               = offset + 4;
+}
+
+void
+XmlBufferedWriter::write(char_t d0_, char_t d1_, // NOLINT(bugprone-easily-swappable-parameters)
+                         char_t d2_, char_t d3_, char_t d4_)
+{
+  size_t offset = bufsize;
+  if(offset > bufcapacity - kShift5) offset = flush();
+
+  buffer.at(offset + 0) = d0_;
+  buffer.at(offset + 1) = d1_;
+  buffer.at(offset + 2) = d2_;
+  buffer.at(offset + 3) = d3_;
+  buffer.at(offset + 4) = d4_;
+  bufsize               = offset + kShift5;
+}
+
+void
+XmlBufferedWriter::write(char_t d0_, char_t d1_, char_t d2_, // NOLINT(bugprone-easily-swappable-parameters)
+                         char_t d3_, char_t d4_, char_t d5_)
+{
+  size_t offset = bufsize;
+  if(offset > bufcapacity - kShift6) offset = flush();
+
+  buffer.at(offset + 0)       = d0_;
+  buffer.at(offset + 1)       = d1_;
+  buffer.at(offset + 2)       = d2_;
+  buffer.at(offset + 3)       = d3_;
+  buffer.at(offset + 4)       = d4_;
+  buffer.at(offset + kShift5) = d5_;
+  bufsize                     = offset + kShift6;
+}
---
+++ lumex/xml/writer/XmlBufferedWriter.hpp
@@ -0,0 +1,83 @@
+#ifndef LUMEX_XML_WRITER_XML_BUFFERED_WRITER_HPP
+#define LUMEX_XML_WRITER_XML_BUFFERED_WRITER_HPP
+
+#include <array>
+#include <cstddef>
+
+#include "lumex/xml/types/XmlTypes.hpp"
+
+#include "IXmlWriter.hpp"
+
+using namespace Lumex::Xml::Types;
+
+namespace Lumex // NOLINT(modernize-concat-nested-namespaces)
+{
+  namespace Xml
+  {
+    namespace Writer
+    {
+
+      class XmlBufferedWriter // NOLINT(cppcoreguidelines-special-member-functions)
+      {
+      public:
+        static constexpr short const kBufCapacity8 = 8;
+        static constexpr short const kShift5       = 5;
+        static constexpr short const kShift6       = 6;
+
+        // utf8 maximum expansion: x4 (-> utf32)
+        // utf16 maximum expansion: x2 (-> utf32)
+        // utf32 maximum expansion: x1
+        enum : std::uint16_t
+        {
+          bufcapacitybytes = 10240,
+          bufcapacity      = bufcapacitybytes / (sizeof(char_t) + 4)
+        };
+
+        std::array<char_t, bufcapacity> buffer; // NOLINT(misc-non-private-member-variables-in-classes)
+
+        union {
+          std::array<std::uint8_t, static_cast<size_t>(4U * bufcapacity)> data_u8;
+          std::array<std::uint16_t, static_cast<size_t>(2U * bufcapacity)> data_u16;
+          std::array<std::uint32_t, bufcapacity> data_u32;
+          std::array<char_t, bufcapacity> data_char;
+        } scratch; // NOLINT(misc-non-private-member-variables-in-classes)
+
+        IXmlWriter &writer;    // NOLINT(misc-non-private-member-variables-in-classes,
+                               // cppcoreguidelines-avoid-const-or-ref-data-members)
+        size_t bufsize{};      // NOLINT(misc-non-private-member-variables-in-classes)
+        xml_encoding encoding; // NOLINT(misc-non-private-member-variables-in-classes)
+
+        XmlBufferedWriter(XmlBufferedWriter const &)            = delete;
+        XmlBufferedWriter &operator=(XmlBufferedWriter const &) = delete;
+
+        XmlBufferedWriter(IXmlWriter &writer_, xml_encoding user_encoding);
+
+        size_t flush();
+
+        void flush(char_t const *data, size_t size);
+
+        void write_direct(char_t const *data, size_t length);
+
+        void write_buffer(char_t const *data, size_t length);
+
+        void write_string(char_t const *data);
+
+        void write(char_t d0_);
+
+        void write(char_t d0_, char_t d1_); // NOLINT(bugprone-easily-swappable-parameters)
+
+        void write(char_t d0_, char_t d1_, char_t d2_); // NOLINT(bugprone-easily-swappable-parameters)
+
+        void write(char_t d0_, char_t d1_, char_t d2_, char_t d3_); // NOLINT(bugprone-easily-swappable-parameters)
+
+        void write(char_t d0_, char_t d1_, // NOLINT(bugprone-easily-swappable-parameters)
+                   char_t d2_, char_t d3_, char_t d4_);
+
+        void write(char_t d0_, char_t d1_, char_t d2_, // NOLINT(bugprone-easily-swappable-parameters)
+                   char_t d3_, char_t d4_, char_t d5_);
+      };
+    } // namespace Writer
+  } // namespace Xml
+} // namespace Lumex
+
+#endif // !LUMEX_XML_WRITER_XML_BUFFERED_WRITER_HPP
