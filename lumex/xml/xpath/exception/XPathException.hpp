83cbba615a3d92a339328609e2a6f70a08b885d1
ViNN280801 <vladislav_semykin01@mail.ru>
2025-08-04 13:59:51 +0300

    fix: removed exception from DLL exportable, because it can't be exportable, exceptions are not supported in libraries
    Signed-off-by: ViNN280801 <vladislav_semykin01@mail.ru>

--- lumex/xml/xpath/exception/XPathException.hpp
+++ lumex/xml/xpath/exception/XPathException.hpp
@@ -1,10 +1,9 @@
 #ifndef LUMEX_XML_XPATH_EXCEPTION_HPP
 #define LUMEX_XML_XPATH_EXCEPTION_HPP
 
-#include "lumex/LumexExport.hpp"
-
 #include <exception>
 
+#include "lumex/core/utility/LumexAssert.hpp"
 #include "lumex/core/utility/LumexAttributes.hpp"
 
 #include "lumex/xml/xpath/parser/XPathParseResult.hpp"
@@ -19,19 +18,27 @@ namespace Lumex // NOLINT(modernize-concat-nested-namespaces)
     {
       namespace Exception
       {
-        class LUMEX_API XPathException : public std::exception
+        class XPathException : public std::exception
         {
         public:
           // Construct exception from parse result
-          explicit XPathException(XPathParseResult const &result);
+          explicit XPathException(XPathParseResult const &result_) : m_result(result_) { LUMEX_ASSERT(m_result.error); }
 
           // Get error message
           LUMEX_ATTRIBUTE_NODISCARD("Error message should not be discarded.")
-          char const *what() const noexcept override;
+          char const *
+          what() const noexcept override
+          {
+            return m_result.error;
+          }
 
           // Get parse result
           LUMEX_ATTRIBUTE_NODISCARD("Parse result should not be discarded.")
-          XPathParseResult const &result() const;
+          XPathParseResult const &
+          result() const
+          {
+            return m_result;
+          }
 
         private:
           XPathParseResult m_result;
